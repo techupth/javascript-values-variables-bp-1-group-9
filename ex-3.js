cb25293e720fa584da723ba2671560f9520e8ad7
Peeraya Natcharoenwong <Peebeary-55@gmail.com>
2023-09-13 13:37:14 +0700

    ex-3-spy

--- ex-3.js
+++ ex-3.js
@@ -1,6 +1,4 @@
-// Exercise #3: Celsius to Fahrenheit
 let celsius = 30;
-
-// Start coding here;
-
-let fahrenheit;
+let fahrenheit = 0;
+fahrenheit = (celsius * 1.8) + 32;
+console.log(fahrenheit);
